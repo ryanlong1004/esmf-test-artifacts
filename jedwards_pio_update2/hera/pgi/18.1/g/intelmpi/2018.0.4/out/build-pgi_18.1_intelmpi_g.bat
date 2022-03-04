bad99eb293d1a67dfa1cfc806b35068cb50fa778
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-04 21:13:14 +0000

    update for build of pgi_18.1_intelmpi_g_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on hera [ci skip]

---
+++ jedwards_pio_update2/hera/pgi/18.1/g/intelmpi/2018.0.4/out/build-pgi_18.1_intelmpi_g.bat
@@ -0,0 +1,27 @@
+Fri Mar 4 21:13:13 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-pgi_18.1_intelmpi_g.bat_%j.o
+#SBATCH -e build-pgi_18.1_intelmpi_g.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
+module load pgi/18.10 impi/2018.0.4 
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_18.1_intelmpi_g_jedwards_pio_update2
+export ESMF_COMPILER=pgi
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
---
+++ jedwards_pio_update2/hera/pgi/18.1/g/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_g.bat
@@ -0,0 +1,33 @@
+Fri Mar 4 21:13:13 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o test-pgi_18.1_intelmpi_g.bat_%j.o
+#SBATCH -e test-pgi_18.1_intelmpi_g.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
+module load pgi/18.10 impi/2018.0.4 
+
+module list >& module-test.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_18.1_intelmpi_g_jedwards_pio_update2
+export ESMF_COMPILER=pgi
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
