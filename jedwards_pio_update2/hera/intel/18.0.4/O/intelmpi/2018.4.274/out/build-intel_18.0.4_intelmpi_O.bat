5ff91a6b88bf967e7cd0047ccdaf162d4f9267f2
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-04 19:37:55 +0000

    update for build of intel_18.0.4_intelmpi_O_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on hera [ci skip]

---
+++ jedwards_pio_update2/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
@@ -0,0 +1,28 @@
+Fri Mar 4 19:37:55 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-intel_18.0.4_intelmpi_O.bat_%j.o
+#SBATCH -e build-intel_18.0.4_intelmpi_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
+module load intel/18.0.5.274 impi/2018.4.274 netcdf/4.7.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_intelmpi_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
