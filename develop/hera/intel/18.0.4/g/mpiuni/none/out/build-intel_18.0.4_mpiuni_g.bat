315e34a407b307d94180b108e8955e02f3f1e3c5
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-04 19:59:05 +0000

    update for build of intel_18.0.4_mpiuni_g_develop with hash v8.3.0b09 on hera [ci skip]

---
+++ develop/hera/intel/18.0.4/g/mpiuni/none/out/build-intel_18.0.4_mpiuni_g.bat
@@ -0,0 +1,27 @@
+Fri Mar 4 19:59:05 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-intel_18.0.4_mpiuni_g.bat_%j.o
+#SBATCH -e build-intel_18.0.4_mpiuni_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load intel/18.0.5.274  netcdf/4.7.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_mpiuni_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
