180eea44dcb5d932a63eef39751b81ee140ea649
Mark A Potts <Mark.Potts@noaa.gov>
2022-03-04 23:46:54 -0500

    update for build of gfortran_8.3.0_mpi_g_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on gaea [ci skip]

---
+++ jedwards_pio_update2/gaea/gfortran/8.3.0/g/mpi/7.7.11/out/test-gfortran_8.3.0_mpi_g.bat
@@ -0,0 +1,37 @@
+Fri Mar 4 23:46:54 EST 2022
+#!/bin/sh -l
+#SBATCH --account=nggps_emc
+#SBATCH -o test-gfortran_8.3.0_mpi_g.bat_%j.o
+#SBATCH -e test-gfortran_8.3.0_mpi_g.bat_%j.e
+#SBATCH --time=3:00:00
+#SBATCH --cluster=c4
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module unload PrgEnv-intel
+
+module load PrgEnv-gnu git/2.26.0
+module load gcc/8.3.0 cray-mpich/7.7.11 cray-netcdf/4.6.3.2
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/lustre/f2/dev/ncep/Mark.Potts/gfortran_8.3.0_mpi_g_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
