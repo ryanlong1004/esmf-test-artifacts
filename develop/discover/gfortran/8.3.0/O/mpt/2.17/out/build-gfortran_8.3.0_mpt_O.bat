57559f1f596f553833c11d8154c83f3d18eb1df6
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-05 01:08:49 -0500

    update for build of gfortran_8.3.0_mpt_O_develop with hash v8.3.0b09 on discover [ci skip]

---
+++ develop/discover/gfortran/8.3.0/O/mpt/2.17/out/build-gfortran_8.3.0_mpt_O.bat
@@ -0,0 +1,26 @@
+Sat Mar 5 01:08:49 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o build-gfortran_8.3.0_mpt_O.bat_%j.o
+#SBATCH -e build-gfortran_8.3.0_mpt_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/gcc/8.3.0 mpi/sgi-mpt/2.17 
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpt_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 2>&1| tee build_$JOBID.log
+
