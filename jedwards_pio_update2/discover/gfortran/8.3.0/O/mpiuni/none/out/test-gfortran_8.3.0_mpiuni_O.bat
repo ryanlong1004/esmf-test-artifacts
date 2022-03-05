b106d950c4632fb6389d14958fa8467d02b98c38
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-05 01:09:46 -0500

    update for build of gfortran_8.3.0_mpiuni_O_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on discover [ci skip]

---
+++ jedwards_pio_update2/discover/gfortran/8.3.0/O/mpiuni/none/out/test-gfortran_8.3.0_mpiuni_O.bat
@@ -0,0 +1,32 @@
+Sat Mar 5 01:09:46 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o test-gfortran_8.3.0_mpiuni_O.bat_%j.o
+#SBATCH -e test-gfortran_8.3.0_mpiuni_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/gcc/8.3.0  
+
+module list >& module-test.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
