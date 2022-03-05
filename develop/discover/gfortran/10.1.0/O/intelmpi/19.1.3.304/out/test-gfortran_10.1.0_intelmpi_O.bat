ddb59df09ad8fd7eeca6729e9d02c2d19b3b8c47
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-05 01:11:13 -0500

    update for build of gfortran_10.1.0_intelmpi_O_develop with hash v8.3.0b09 on discover [ci skip]

---
+++ develop/discover/gfortran/10.1.0/O/intelmpi/19.1.3.304/out/test-gfortran_10.1.0_intelmpi_O.bat
@@ -0,0 +1,33 @@
+Sat Mar 5 01:11:13 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o test-gfortran_10.1.0_intelmpi_O.bat_%j.o
+#SBATCH -e test-gfortran_10.1.0_intelmpi_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/gcc/10.1.0 mpi/impi/19.1.3.304 
+
+module list >& module-test.log
+
+set -x
+
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_10.1.0_intelmpi_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
