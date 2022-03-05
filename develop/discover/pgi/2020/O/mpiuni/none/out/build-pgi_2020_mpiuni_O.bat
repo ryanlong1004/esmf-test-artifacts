9d54cb70b3fe9b29c0e4327598917f0a712b541c
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-05 01:37:29 -0500

    update for build of pgi_2020_mpiuni_O_develop with hash v8.3.0b09 on discover [ci skip]

---
+++ develop/discover/pgi/2020/O/mpiuni/none/out/build-pgi_2020_mpiuni_O.bat
@@ -0,0 +1,27 @@
+Sat Mar 5 01:37:29 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o build-pgi_2020_mpiuni_O.bat_%j.o
+#SBATCH -e build-pgi_2020_mpiuni_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+module load comp/pgi/20.4  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_mpiuni_O_develop
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 2>&1| tee build_$JOBID.log
+
