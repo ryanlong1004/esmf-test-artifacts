1ec62d60965fa4faf7a19bfb805d1f404f5e0198
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-05 01:15:48 -0500

    update for build of gfortran_8.3.0_mpiuni_g_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on discover [ci skip]

---
+++ jedwards_pio_update2/discover/gfortran/8.3.0/g/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -0,0 +1,26 @@
+Sat Mar 5 01:15:48 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
+#SBATCH -e build-gfortran_8.3.0_mpiuni_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/gcc/8.3.0  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpiuni_g_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 2>&1| tee build_$JOBID.log
+
