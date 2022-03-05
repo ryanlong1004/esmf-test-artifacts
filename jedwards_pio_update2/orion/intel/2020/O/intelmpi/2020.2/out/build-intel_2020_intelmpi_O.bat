e43347530d681d848024d8ce6fc2af6bc08d39c5
Mark Potts <mpotts@redlineperf.com>
2022-03-05 02:36:16 -0600

    update for build of intel_2020_intelmpi_O_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on orion [ci skip]

---
+++ jedwards_pio_update2/orion/intel/2020/O/intelmpi/2020.2/out/build-intel_2020_intelmpi_O.bat
@@ -0,0 +1,29 @@
+Sat Mar 5 02:36:16 CST 2022
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o build-intel_2020_intelmpi_O.bat_%j.o
+#SBATCH -e build-intel_2020_intelmpi_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load intelpython3 cmake
+module load intel/2020.2 impi/2020.2 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020_intelmpi_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
