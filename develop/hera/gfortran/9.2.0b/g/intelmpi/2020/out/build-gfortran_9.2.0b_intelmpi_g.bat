c1d2a97b6ce051cefeceb9bf39d30f31aadb29ab
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-07 06:39:57 +0000

    update for test of gfortran_9.2.0b_intelmpi_g_develop with hash v8.3.0b09 on hera [ci skip]

--- develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/build-gfortran_9.2.0b_intelmpi_g.bat
+++ develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/build-gfortran_9.2.0b_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon Mar 7 06:17:42 UTC 2022
+Mon Mar 7 06:39:47 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-gfortran_9.2.0b_intelmpi_g.bat_%j.o
--- develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/test-gfortran_9.2.0b_intelmpi_g.bat
+++ develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/test-gfortran_9.2.0b_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon Mar 7 06:17:42 UTC 2022
+Mon Mar 7 06:39:47 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0b_intelmpi_g.bat_%j.o
