9c1a4d0228ce848134ea8555712557ec9fba57be
Ryan Long <ryan.long@noaa.gov>
2022-03-08 11:42:58 -0700

    update for build of intel_18.0.5_openmpi_O_jedwards_pio_update2 with hash v8.3.0b08-93-g7b74714 on cheyenne [ci skip]

---
+++ jedwards_pio_update2/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
@@ -0,0 +1,25 @@
+Tue Mar 8 11:42:58 MST 2022
+#!/bin/sh -l
+#PBS -N build-intel_18.0.5_openmpi_O.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_O_jedwards_pio_update2
+module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
