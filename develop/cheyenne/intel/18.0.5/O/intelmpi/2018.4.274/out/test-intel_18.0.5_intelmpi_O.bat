3f55aacdfd0f739def69793b50e5ad9460d231be
Ryan Long <ryan.long@noaa.gov>
2022-03-08 11:42:36 -0700

    update for build of intel_18.0.5_intelmpi_O_develop with hash v8.3.0b09 on cheyenne [ci skip]

---
+++ develop/cheyenne/intel/18.0.5/O/intelmpi/2018.4.274/out/test-intel_18.0.5_intelmpi_O.bat
@@ -0,0 +1,39 @@
+Tue Mar 8 11:42:36 MST 2022
+#!/bin/sh -l
+#PBS -N test-intel_18.0.5_intelmpi_O.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_intelmpi_O_develop
+module load intel/18.0.5 impi/2018.4.274 netcdf/4.6.3
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_intelmpi_O_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
+
+cd ../src/addon/ESMPy
+
+export PATH=$PATH:$HOME/.local/bin
+python3 setup.py build 2>&1 | tee python_build.log
+ssh cheyenne6 /glade/scratch/rlong/esmf-testing/intel_18.0.5_intelmpi_O_develop/runpython.sh 2>&1 | tee python_build.log
+python3 setup.py test 2>&1 | tee python_test.log
+python3 setup.py test_examples 2>&1 | tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
