a731dbcbbf802119925711df7e9e410316267536
Ryan Long <ryan.long@noaa.gov>
2022-03-08 11:37:02 -0700

    update for build of intel_18.0.5_mpiuni_g_jedwards_pio_update2 with hash v8.3.0b08-93-g7b74714 on cheyenne [ci skip]

---
+++ jedwards_pio_update2/cheyenne/intel/18.0.5/g/mpiuni/none/out/test-intel_18.0.5_mpiuni_g.bat
@@ -0,0 +1,30 @@
+Tue Mar 8 11:37:02 MST 2022
+#!/bin/sh -l
+#PBS -N test-intel_18.0.5_mpiuni_g.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_g_jedwards_pio_update2
+module load intel/18.0.5 none netcdf/4.6.3
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_g_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
