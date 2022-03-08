c989b633cb5e89878cacd4e555c6e94a349a3d91
Ryan Long <ryan.long@noaa.gov>
2022-03-08 11:45:08 -0700

    update for build of intel_18.0.5_mpt_g_develop with hash v8.3.0b09 on cheyenne [ci skip]

---
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
@@ -0,0 +1,25 @@
+Tue Mar 8 11:45:08 MST 2022
+#!/bin/sh -l
+#PBS -N build-intel_18.0.5_mpt_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+module load intel/18.0.5 mpt/2.19 netcdf/4.6.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
---
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
@@ -0,0 +1,30 @@
+Tue Mar 8 11:45:08 MST 2022
+#!/bin/sh -l
+#PBS -N test-intel_18.0.5_mpt_g.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+module load intel/18.0.5 mpt/2.19 netcdf/4.6.3
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
