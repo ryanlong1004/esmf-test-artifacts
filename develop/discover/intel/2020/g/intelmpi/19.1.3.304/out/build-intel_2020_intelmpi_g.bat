4e8556198ea1a73eed83fb9f3b45f47b982fdbc3
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-09 01:19:57 -0500

    update for build of intel_2020_intelmpi_g_develop with hash v8.3.0b09 on discover [ci skip]

--- develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
+++ develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 8 02:06:42 EST 2022
+Wed Mar 9 01:19:57 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
--- develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
+++ develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 8 02:06:42 EST 2022
+Wed Mar 9 01:19:57 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_g.bat_%j.o
