ff5b6f738ad6749c81910f3a5792a0c05a61aa9f
Rocky Dunlap <dunlap@ucar.edu>
2022-03-08 22:44:16 -0800

    update for build of intel_19.0.3_mpiuni_O_jedwards_pio_update2 with hash v8.3.0b08-94-gc0ddbab on cori [ci skip]

---
+++ jedwards_pio_update2/cori/intel/19.0.3/O/mpiuni/none/out/build-intel_19.0.3_mpiuni_O.bat
@@ -0,0 +1,28 @@
+Tue Mar 8 22:44:15 PST 2022
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o build-intel_19.0.3_mpiuni_O.bat_%j.o
+#SBATCH -e build-intel_19.0.3_mpiuni_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load intel/19.0.3.199  cray-netcdf/4.6.3.2
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdf"
+export ESMF_NETCDFF_LIBS="-lnetcdff"
+export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.0.3_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 32 2>&1| tee build_$JOBID.log
+
---
+++ jedwards_pio_update2/cori/intel/19.0.3/O/mpiuni/none/out/test-intel_19.0.3_mpiuni_O.bat
@@ -0,0 +1,34 @@
+Tue Mar 8 22:44:15 PST 2022
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o test-intel_19.0.3_mpiuni_O.bat_%j.o
+#SBATCH -e test-intel_19.0.3_mpiuni_O.bat_%j.e
+#SBATCH --time=3:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load intel/19.0.3.199  cray-netcdf/4.6.3.2
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdf"
+export ESMF_NETCDFF_LIBS="-lnetcdff"
+export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.0.3_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
