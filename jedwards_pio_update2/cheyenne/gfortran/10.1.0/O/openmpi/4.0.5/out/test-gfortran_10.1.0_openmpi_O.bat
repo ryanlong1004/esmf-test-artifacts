5e3228decd4d18a335e230e54df98192264f6e3c
Ryan Long <ryan.long@noaa.gov>
2022-03-10 15:28:54 -0700

    update for build of gfortran_10.1.0_openmpi_O_jedwards_pio_update2 with hash v8.3.0b09-93-ga567ee5 on cheyenne [ci skip]

---
+++ jedwards_pio_update2/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
@@ -0,0 +1,32 @@
+Thu Mar 10 15:28:53 MST 2022
+#!/bin/sh -l
+#PBS -N test-gfortran_10.1.0_openmpi_O.bat
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/gfortran_10.1.0_openmpi_O_jedwards_pio_update2
+module load gnu/10.1.0 openmpi/4.0.5 netcdf/4.7.4
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_10.1.0_openmpi_O_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
