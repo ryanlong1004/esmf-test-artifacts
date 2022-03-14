d35483303ba9c52f4d2031f765d552f9d2c44fb1
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-14 01:14:41 -0400

    update for build of intel_2020_intelmpi_O_jedwards_pio_update2 with hash v8.3.0b09-96-g559b459 on discover [ci skip]

--- jedwards_pio_update2/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
+++ jedwards_pio_update2/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Mar 13 01:44:07 EST 2022
+Mon Mar 14 01:14:39 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_O.bat_%j.o
--- jedwards_pio_update2/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
+++ jedwards_pio_update2/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Mar 13 01:55:02 EST 2022
+Mon Mar 14 01:14:38 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_O.bat_%j.o
