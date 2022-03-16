cf7c5b375d4a1e71d95a77ecc6aca3e0f0d250dc
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-16 01:14:34 -0400

    update for build of intel_19.1.3_mpt_O_jedwards_pio_update2 with hash v8.3.0b09-100-g5ff85a9 on discover [ci skip]

---
+++ jedwards_pio_update2/discover/intel/19.1.3/O/mpt/2.17/out/test-intel_19.1.3_mpt_O.bat
@@ -0,0 +1,36 @@
+Wed Mar 16 01:14:32 EDT 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o test-intel_19.1.3_mpt_O.bat_%j.o
+#SBATCH -e test-intel_19.1.3_mpt_O.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export MPICXX_CXX=icpc
+export MPICC_CC=icc
+module load comp/intel/19.1.3.304 mpi/sgi-mpt/2.17 netcdf4/4.7.4
+module load hdf5/1.13.0 
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lhdf5_hl -lhdf5"
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/intel_19.1.3_mpt_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
