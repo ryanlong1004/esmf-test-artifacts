c7bcad3529abc32dab13529703da5472154fd3dc
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-17 04:07:41 +0000

    update for build of pgi_18.1_intelmpi_g_develop with hash v8.3.0b09-3-ge20ebbf on hera [ci skip]

--- develop/hera/pgi/18.1/g/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_g.bat
+++ develop/hera/pgi/18.1/g/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 15 12:13:01 UTC 2022
+Thu Mar 17 04:07:40 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-pgi_18.1_intelmpi_g.bat_%j.o
@@ -10,6 +10,8 @@ Tue Mar 15 12:13:01 UTC 2022
 #SBATCH --ntasks-per-node=40
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
+
+module load cmake
 export ESMF_MPIRUN=mpirun.srun
 module load pgi/18.10 impi/2018.0.4 
 
