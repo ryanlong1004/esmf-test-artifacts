67e34be2dfc47d3e45dcd01dec9efa496120773d
Mark A Potts <Mark.Potts@noaa.gov>
2022-03-17 01:30:42 +0000

    update for build of gfortran_10.3.0_mpi_g_jedwards_pio_update2 with hash v8.3.0b09-100-g5ff85a9 on acorn [ci skip]

---
+++ jedwards_pio_update2/acorn/gfortran/10.3.0/g/mpi/8.1.7/out/build-gfortran_10.3.0_mpi_g.bat
@@ -0,0 +1,35 @@
+Thu 17 Mar 01:30:42 UTC 2022
+#!/bin/sh -l
+#PBS -N build-gfortran_10.3.0_mpi_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=1:00:00
+#PBS -q dev
+#PBS -A GFS-DEV
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_g_jedwards_pio_update2
+
+module unload PrgEnv-cray PrgEnv-intel
+
+module load PrgEnv-gnu cray-pals craype cmake
+module load gcc/10.3.0 cray-mpich/8.1.7 netcdf
+module load hdf5 
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_MPIRUN=mpirun.unicos
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_CXXLINKOPTS="-fPIC -lnetcdff -lnetcdff"
+export ESMF_NETCDF=nc-config
+sed -i 's/aprun/mpiexec/' scripts/mpirun.unicos
+export ESMF_DIR=/lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_g_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 128 2>&1| tee build_$JOBID.log
+
