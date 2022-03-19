05e048aa40d388ef8d5b4e08fb9c8e2bdca88ccc
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-19 01:31:35 -0400

    update for test of gfortran_8.3.0_mpt_O_develop with hash v8.3.0b09-4-g424b2f2 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/O/mpt/2.17/out/build-gfortran_8.3.0_mpt_O.bat
+++ develop/discover/gfortran/8.3.0/O/mpt/2.17/out/build-gfortran_8.3.0_mpt_O.bat
@@ -1,4 +1,4 @@
-Sat Mar 19 01:08:13 EDT 2022
+Sat Mar 19 01:31:17 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpt_O.bat_%j.o
--- jedwards_pio_update2/discover/gfortran/8.3.0/O/mpt/2.17/out/build-gfortran_8.3.0_mpt_O.bat
+++ jedwards_pio_update2/discover/gfortran/8.3.0/O/mpt/2.17/out/build-gfortran_8.3.0_mpt_O.bat
@@ -1,4 +1,4 @@
-Sat Mar 19 01:07:47 EDT 2022
+Sat Mar 19 01:31:23 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpt_O.bat_%j.o
