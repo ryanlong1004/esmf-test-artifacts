aee43465c5ea21b2227a374b6f2089d12701866b
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-19 01:35:54 -0400

    update for build of pgi_2020_mpiuni_O_jedwards_pio_update2 with hash v8.3.0b09-102-gabaf3ef on discover [ci skip]

--- jedwards_pio_update2/discover/pgi/2020/O/mpiuni/none/out/build-pgi_2020_mpiuni_O.bat
+++ jedwards_pio_update2/discover/pgi/2020/O/mpiuni/none/out/build-pgi_2020_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Fri Mar 18 02:49:26 EDT 2022
+Sat Mar 19 01:35:53 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_2020_mpiuni_O.bat_%j.o
--- jedwards_pio_update2/discover/pgi/2020/O/mpiuni/none/out/test-pgi_2020_mpiuni_O.bat
+++ jedwards_pio_update2/discover/pgi/2020/O/mpiuni/none/out/test-pgi_2020_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Fri Mar 18 02:49:26 EDT 2022
+Sat Mar 19 01:35:54 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-pgi_2020_mpiuni_O.bat_%j.o
