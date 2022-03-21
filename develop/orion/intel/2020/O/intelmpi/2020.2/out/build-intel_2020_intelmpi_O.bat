11512fb9261e09753e65721e97d1cdebc62194c5
Mark Potts <mpotts@redlineperf.com>
2022-03-21 02:33:25 -0500

    update for build of gfortran_8.3.0_openmpi_O_develop with hash v8.3.0b09-4-g424b2f2 on orion [ci skip]

--- develop/orion/intel/2020/O/intelmpi/2020.2/out/build-intel_2020_intelmpi_O.bat
+++ develop/orion/intel/2020/O/intelmpi/2020.2/out/build-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Mar 20 07:00:07 CDT 2022
+Mon Mar 21 02:32:53 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_2020_intelmpi_O.bat_%j.o
--- jedwards_pio_update2/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/test-gfortran_8.3.0_openmpi_O.bat
+++ jedwards_pio_update2/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/test-gfortran_8.3.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Mar 20 03:41:16 CDT 2022
+Mon Mar 21 02:32:50 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_8.3.0_openmpi_O.bat_%j.o
