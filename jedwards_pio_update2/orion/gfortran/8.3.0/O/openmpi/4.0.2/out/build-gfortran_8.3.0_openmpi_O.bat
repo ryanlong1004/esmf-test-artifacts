2b3731cd6ad83d952784c6d700c5425bc9ce2cc8
Mark Potts <mpotts@redlineperf.com>
2022-03-22 02:35:49 -0500

    update for build of gfortran_8.3.0_openmpi_O_jedwards_pio_update2 with hash v8.3.0b09-102-gabaf3ef on orion [ci skip]

--- jedwards_pio_update2/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_O.bat
+++ jedwards_pio_update2/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Mon Mar 21 02:58:52 CDT 2022
+Tue Mar 22 02:35:48 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_openmpi_O.bat_%j.o
@@ -12,6 +12,7 @@ Mon Mar 21 02:58:52 CDT 2022
 export JOBID=$SLURM_JOBID
 
 module load intelpython3 cmake
+export ESMF_MPIRUN=mpirun.srun
 module load gcc/8.3.0 openmpi/4.0.2 netcdf/4.7.4
 module list >& module-build.log
 
