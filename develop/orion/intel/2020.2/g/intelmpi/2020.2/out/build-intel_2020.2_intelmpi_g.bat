1c4c6c5b63162c5dce9f20d8288490eac4914eab
Mark Potts <mpotts@redlineperf.com>
2022-03-22 02:33:50 -0500

    update for build of intel_2020.2_intelmpi_g_develop with hash v8.3.0b09-5-g496f893 on orion [ci skip]

---
+++ develop/orion/intel/2020.2/g/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_g.bat
@@ -0,0 +1,30 @@
+Tue Mar 22 02:33:49 CDT 2022
+#!/bin/sh -l
+#SBATCH --account=da-cpu
+#SBATCH -o build-intel_2020.2_intelmpi_g.bat_%j.o
+#SBATCH -e build-intel_2020.2_intelmpi_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load intelpython3 cmake
+export ESMF_MPIRUN=mpirun.srun
+module load intel/2020.2 impi/2020.2 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020.2_intelmpi_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
