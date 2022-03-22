59aaf910b3433bf63f2046cb38dfb97d8024515a
Mark Potts <mpotts@redlineperf.com>
2022-03-22 08:37:13 -0500

    update for test of intel_2020.2_intelmpi_O_develop with hash v8.3.0b09-5-g496f893 on orion [ci skip]

--- develop/orion/intel/2020.2/O/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_O.bat
+++ develop/orion/intel/2020.2/O/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Tue Mar 22 02:32:48 CDT 2022
+Tue Mar 22 08:32:58 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_2020.2_intelmpi_O.bat_%j.o
--- develop/orion/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
+++ develop/orion/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 22 02:33:49 CDT 2022
+Tue Mar 22 08:34:32 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_2020.2_intelmpi_g.bat_%j.o
