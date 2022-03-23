3432fee35d541ce29f393c697b2bfe9b7216752d
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-23 21:36:54 +0000

    update for build of pgi_19.1_intelmpi_O_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on hera [ci skip]

---
+++ jedwards_pio_update2/hera/pgi/19.1/O/intelmpi/2019.0.5/out/build-pgi_19.1_intelmpi_O.bat
@@ -0,0 +1,29 @@
+Wed Mar 23 21:36:53 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-pgi_19.1_intelmpi_O.bat_%j.o
+#SBATCH -e build-pgi_19.1_intelmpi_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+module load pgi/19.10 impi/2019.0.5 
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_O_jedwards_pio_update2
+export ESMF_COMPILER=pgi
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
