df32559ddcef565853fd829abe386d8b242968ae
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-23 23:51:40 +0000

    update for test of pgi_19.1_intelmpi_g_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on hera [ci skip]

---
+++ jedwards_pio_update2/hera/pgi/19.1/g/intelmpi/2019.0.5/lib/esmf.mk
@@ -0,0 +1,120 @@
+build time -- 2022-03-23 21:50:21
+# ESMF application makefile fragment
+#
+# Use the following ESMF_ variables to compile and link
+# your ESMF application against this ESMF build.
+#
+# !!! VERY IMPORTANT: If the location of this ESMF build is   !!!
+# !!! changed, e.g. libesmf.a is copied to another directory, !!!
+# !!! this file - esmf.mk - must be edited to adjust to the   !!!
+# !!! correct new path                                        !!!
+#
+# Please see end of file for options used on this ESMF build
+#
+
+#----------------------------------------------
+ESMF_VERSION_STRING=8.3.0 beta snapshot
+ESMF_VERSION_STRING_GIT=v8.3.0b09-103-gbb860f29f6
+#----------------------------------------------
+
+ESMF_VERSION_MAJOR=8
+ESMF_VERSION_MINOR=3
+ESMF_VERSION_REVISION=0
+ESMF_VERSION_PATCHLEVEL=0
+ESMF_VERSION_PUBLIC='F'
+ESMF_VERSION_BETASNAPSHOT='T'
+
+
+ESMF_APPSDIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/apps/appsg/Linux.pgi.64.intelmpi.default
+ESMF_LIBSDIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default
+
+
+ESMF_F90COMPILER=mpipgf90
+ESMF_F90LINKER=mpipgf90
+
+ESMF_F90COMPILEOPTS=-g -fpic -Mallocatable=03 -lpthread
+ESMF_F90COMPILEPATHS=-I/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/mod/modg/Linux.pgi.64.intelmpi.default -I/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/src/include 
+ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='v8.3.0b09-103-gbb860f29f6' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENMP -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_g -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2 -DESMF_PGIVERSION_MAJOR=19 -DESMF_PGIVERSION_MINOR=10 -DESMF_PGIVERSION_PATCH=0 -DESMF_NO_F2018ASSUMEDTYPE
+ESMF_F90COMPILEFREECPP=
+ESMF_F90COMPILEFREENOCPP=-Mfreeform
+ESMF_F90COMPILEFIXCPP=-Mpreprocess -Mnofreeform
+ESMF_F90COMPILEFIXNOCPP=
+
+ESMF_F90LINKOPTS=  -lpthread -Wl,--no-as-needed
+ESMF_F90LINKPATHS=-L/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default  -L/apps/pgi/linux86-64-llvm/19.10/libso
+ESMF_F90ESMFLINKPATHS=-L/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default
+ESMF_F90LINKRPATHS=-Wl,-rpath,/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default  -Wl,-rpath,/apps/pgi/linux86-64-llvm/19.10/libso
+ESMF_F90ESMFLINKRPATHS=-Wl,-rpath,/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default
+ESMF_F90LINKLIBS= -pgc++libs -ldl
+ESMF_F90ESMFLINKLIBS=-lesmf  -pgc++libs -ldl
+
+ESMF_CXXCOMPILER=mpipgc++
+ESMF_CXXLINKER=mpipgc++
+
+ESMF_CXXCOMPILEOPTS=-std=c++11 -g -fpic -lpthread
+ESMF_CXXCOMPILEPATHS= -I/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/src/include   -I/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/src/prologue/yaml-cpp/include
+ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='v8.3.0b09-103-gbb860f29f6' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENMP -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_g -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2 -D__SDIR__='' -DESMF_CXXSTD=11 -DESMF_PGIVERSION_MAJOR=19 -DESMF_PGIVERSION_MINOR=10 -DESMF_PGIVERSION_PATCH=0 -DESMF_NO_F2018ASSUMEDTYPE
+
+ESMF_CXXLINKOPTS=  -lpthread -Wl,--no-as-needed
+ESMF_CXXLINKPATHS=-L/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default  -L/apps/pgi/linux86-64-llvm/19.10/libso
+ESMF_CXXESMFLINKPATHS=-L/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default
+ESMF_CXXLINKRPATHS=-Wl,-rpath,/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default  -Wl,-rpath,/apps/pgi/linux86-64-llvm/19.10/libso
+ESMF_CXXESMFLINKRPATHS=-Wl,-rpath,/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default
+ESMF_CXXLINKLIBS= -pgf90libs -ldl
+ESMF_CXXESMFLINKLIBS=-lesmf  -pgf90libs -ldl
+
+ESMF_SO_F90COMPILEOPTS=-fpic
+ESMF_SO_F90LINKOPTS=-shared
+ESMF_SO_F90LINKOPTSEXE=-Wl,-export-dynamic
+ESMF_SO_CXXCOMPILEOPTS=-fpic
+ESMF_SO_CXXLINKOPTS=-shared
+ESMF_SO_CXXLINKOPTSEXE=-Wl,-export-dynamic
+
+ESMF_OPENMP_F90COMPILEOPTS= -mp
+ESMF_OPENMP_F90LINKOPTS= -mp
+ESMF_OPENMP_CXXCOMPILEOPTS= -mp --exceptions
+ESMF_OPENMP_CXXLINKOPTS= -mp --exceptions
+
+ESMF_OPENACC_F90COMPILEOPTS= -acc -Minfo
+ESMF_OPENACC_F90LINKOPTS= -acc -Minfo
+ESMF_OPENACC_CXXCOMPILEOPTS= -acc -Minfo
+ESMF_OPENACC_CXXLINKOPTS= -acc -Minfo
+
+# ESMF Tracing compile/link options
+ESMF_TRACE_LDPRELOAD=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/lib/libg/Linux.pgi.64.intelmpi.default/libesmftrace_preload.so
+ESMF_TRACE_STATICLINKOPTS=-static -Wl,--wrap=c_esmftrace_notify_wrappers -Wl,--wrap=c_esmftrace_isinitialized -Wl,--wrap=write -Wl,--wrap=writev -Wl,--wrap=pwrite -Wl,--wrap=read -Wl,--wrap=open -Wl,--wrap=MPI_Allgather -Wl,--wrap=MPI_Allgatherv -Wl,--wrap=MPI_Allreduce -Wl,--wrap=MPI_Alltoall -Wl,--wrap=MPI_Alltoallv -Wl,--wrap=MPI_Alltoallw -Wl,--wrap=MPI_Barrier -Wl,--wrap=MPI_Bcast -Wl,--wrap=MPI_Bsend -Wl,--wrap=MPI_Gather -Wl,--wrap=MPI_Gatherv -Wl,--wrap=MPI_Iprobe -Wl,--wrap=MPI_Irecv -Wl,--wrap=MPI_Irsend -Wl,--wrap=MPI_Isend -Wl,--wrap=MPI_Issend -Wl,--wrap=MPI_Probe -Wl,--wrap=MPI_Recv -Wl,--wrap=MPI_Reduce -Wl,--wrap=MPI_Rsend -Wl,--wrap=MPI_Scan -Wl,--wrap=MPI_Scatter -Wl,--wrap=MPI_Scatterv -Wl,--wrap=MPI_Send -Wl,--wrap=MPI_Sendrecv -Wl,--wrap=MPI_Test -Wl,--wrap=MPI_Testall -Wl,--wrap=MPI_Testany -Wl,--wrap=MPI_Testsome -Wl,--wrap=MPI_Wait -Wl,--wrap=MPI_Waitall -Wl,--wrap=MPI_Waitany -Wl,--wrap=MPI_Waitsome -Wl,--wrap=mpi_allgather_ -Wl,--wrap=mpi_allgather__ -Wl,--wrap=mpi_allgatherv_ -Wl,--wrap=mpi_allgatherv__ -Wl,--wrap=mpi_allreduce_ -Wl,--wrap=mpi_allreduce__ -Wl,--wrap=mpi_alltoall_ -Wl,--wrap=mpi_alltoall__ -Wl,--wrap=mpi_alltoallv_ -Wl,--wrap=mpi_alltoallv__ -Wl,--wrap=mpi_alltoallw_ -Wl,--wrap=mpi_alltoallw__ -Wl,--wrap=mpi_barrier_ -Wl,--wrap=mpi_barrier__ -Wl,--wrap=mpi_bcast_ -Wl,--wrap=mpi_bcast__ -Wl,--wrap=mpi_bsend_ -Wl,--wrap=mpi_bsend__ -Wl,--wrap=mpi_exscan_ -Wl,--wrap=mpi_exscan__ -Wl,--wrap=mpi_gather_ -Wl,--wrap=mpi_gather__ -Wl,--wrap=mpi_gatherv_ -Wl,--wrap=mpi_gatherv__ -Wl,--wrap=mpi_iprobe_ -Wl,--wrap=mpi_iprobe__ -Wl,--wrap=mpi_irecv_ -Wl,--wrap=mpi_irecv__ -Wl,--wrap=mpi_irsend_ -Wl,--wrap=mpi_irsend__ -Wl,--wrap=mpi_isend_ -Wl,--wrap=mpi_isend__ -Wl,--wrap=mpi_issend_ -Wl,--wrap=mpi_issend__ -Wl,--wrap=mpi_probe_ -Wl,--wrap=mpi_probe__ -Wl,--wrap=mpi_recv_ -Wl,--wrap=mpi_recv__ -Wl,--wrap=mpi_reduce_ -Wl,--wrap=mpi_reduce__ -Wl,--wrap=mpi_reduce_scatter_ -Wl,--wrap=mpi_reduce_scatter__ -Wl,--wrap=mpi_rsend_ -Wl,--wrap=mpi_rsend__ -Wl,--wrap=mpi_scatter_ -Wl,--wrap=mpi_scatter__ -Wl,--wrap=mpi_scatterv_ -Wl,--wrap=mpi_scatterv__ -Wl,--wrap=mpi_scan_ -Wl,--wrap=mpi_scan__ -Wl,--wrap=mpi_send_ -Wl,--wrap=mpi_send__ -Wl,--wrap=mpi_sendrecv_ -Wl,--wrap=mpi_sendrecv__ -Wl,--wrap=mpi_test_ -Wl,--wrap=mpi_test__ -Wl,--wrap=mpi_testall_ -Wl,--wrap=mpi_testall__ -Wl,--wrap=mpi_testany_ -Wl,--wrap=mpi_testany__ -Wl,--wrap=mpi_testsome_ -Wl,--wrap=mpi_testsome__ -Wl,--wrap=mpi_wait_ -Wl,--wrap=mpi_wait__ -Wl,--wrap=mpi_waitall_ -Wl,--wrap=mpi_waitall__ -Wl,--wrap=mpi_waitany_ -Wl,--wrap=mpi_waitany__
+ESMF_TRACE_STATICLINKLIBS=-lesmftrace_static
+
+# Internal ESMF variables, do NOT depend on these!
+
+ESMF_INTERNAL_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2
+ESMF_INTERNAL_MPIRUN=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2/scripts/mpirun.srun 
+
+#
+# !!! The following options were used on this ESMF build !!!
+#
+# ESMF_DIR: /scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_intelmpi_g_jedwards_pio_update2
+# ESMF_OS: Linux
+# ESMF_MACHINE: x86_64
+# ESMF_ABI: 64
+# ESMF_COMPILER: pgi
+# ESMF_BOPT: g
+# ESMF_COMM: intelmpi
+# ESMF_SITE: default
+# ESMF_PTHREADS: ON
+# ESMF_OPENMP: OFF
+# ESMF_OPENACC: OFF
+# ESMF_ARRAY_LITE: FALSE
+# ESMF_NO_INTEGER_1_BYTE: TRUE
+# ESMF_NO_INTEGER_2_BYTE: TRUE
+# ESMF_FORTRANSYMBOLS: default
+# ESMF_MAPPER_BUILD: OFF
+# ESMF_AUTO_LIB_BUILD: ON
+# ESMF_DEFER_LIB_BUILD: ON
+# ESMF_SHARED_LIB_BUILD: ON
+# 
+# ESMF environment variables pointing to 3rd party software:
+# ESMF_MOAB:              internal
+# ESMF_LAPACK:            internal
+# ESMF_ACC_SOFTWARE_STACK:            none
+# ESMF_YAMLCPP:           internal
--- jedwards_pio_update2/hera/pgi/19.1/g/intelmpi/2019.0.5/out/test-pgi_19.1_intelmpi_g.bat
+++ jedwards_pio_update2/hera/pgi/19.1/g/intelmpi/2019.0.5/out/test-pgi_19.1_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 21:50:28 UTC 2022
+Wed Mar 23 23:51:30 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-pgi_19.1_intelmpi_g.bat_%j.o
