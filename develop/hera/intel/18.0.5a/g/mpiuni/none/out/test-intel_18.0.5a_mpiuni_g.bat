9b90415f82c31b4a2237cb00d17e1422ff203409
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-23 22:01:46 +0000

    update for build of intel_18.0.5a_mpiuni_g_develop with hash v8.3.0b09-6-gdac1bc5 on hera [ci skip]

---
+++ develop/hera/intel/18.0.5a/g/mpiuni/none/out/test-intel_18.0.5a_mpiuni_g.bat
@@ -0,0 +1,35 @@
+Wed Mar 23 22:01:44 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o test-intel_18.0.5a_mpiuni_g.bat_%j.o
+#SBATCH -e test-intel_18.0.5a_mpiuni_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+module load intel/18.0.5.274  netcdf/4.6.1
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.5a_mpiuni_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
