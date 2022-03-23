fd5b5489e35bc6e82d0ff85daf24f537a5e3513c
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-23 02:11:59 -0400

    update for test of intel_19.1.3_mpt_g_develop with hash v8.3.0b09-6-gdac1bc5 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/build-gfortran_8.3.0_intelmpi_g.bat
+++ develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/build-gfortran_8.3.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 01:34:58 EDT 2022
+Wed Mar 23 02:11:35 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_intelmpi_g.bat_%j.o
--- develop/discover/intel/19.1.3/g/mpt/2.17/out/test-intel_19.1.3_mpt_g.bat
+++ develop/discover/intel/19.1.3/g/mpt/2.17/out/test-intel_19.1.3_mpt_g.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 01:35:55 EDT 2022
+Wed Mar 23 02:11:33 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_19.1.3_mpt_g.bat_%j.o
