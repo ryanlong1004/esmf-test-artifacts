b8475210e506b32a66e7aa05f29848e3d29254aa
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-23 21:17:39 +0000

    update for build of pgi_19.1_mpiuni_O_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on hera [ci skip]

---
+++ jedwards_pio_update2/hera/pgi/19.1/O/mpiuni/none/out/test-pgi_19.1_mpiuni_O.bat
@@ -0,0 +1,34 @@
+Wed Mar 23 21:17:38 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o test-pgi_19.1_mpiuni_O.bat_%j.o
+#SBATCH -e test-pgi_19.1_mpiuni_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+module load pgi/19.10  
+
+module list >& module-test.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_19.1_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
