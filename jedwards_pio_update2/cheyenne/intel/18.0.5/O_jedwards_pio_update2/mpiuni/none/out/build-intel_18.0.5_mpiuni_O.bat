32c6ba072d3c5c813a5d75b37d14776ec2f3f293
Ryan Long <ryan.long@noaa.gov>
2022-03-24 17:38:16 -0600

    update for build of intel_18.0.5_mpiuni_O_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on v8.3.0b09-103-gbb860f2 [ci skip]

---
+++ jedwards_pio_update2/cheyenne/intel/18.0.5/O_jedwards_pio_update2/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
@@ -0,0 +1,28 @@
+Thu Mar 24 17:38:14 MDT 2022
+#!/bin/sh -l
+#PBS -N build-intel_18.0.5_mpiuni_O.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_jedwards_pio_update2
+
+module load python
+export ESMF_MPIRUN=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_jedwards_pio_update2/src/Infrastructure/stubs/mpiuni/mpirun
+module load intel/18.0.5  netcdf/4.6.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
---
+++ jedwards_pio_update2/cheyenne/intel/18.0.5/O_jedwards_pio_update2/mpiuni/none/out/test-intel_18.0.5_mpiuni_O.bat
@@ -0,0 +1,32 @@
+Thu Mar 24 17:38:14 MDT 2022
+#!/bin/sh -l
+#PBS -N test-intel_18.0.5_mpiuni_O.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_jedwards_pio_update2
+
+module load python
+module load intel/18.0.5  netcdf/4.6.3
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
