ccec088c5fb15b9a21197bf224fd594f96bca0b3
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-24 05:55:20 +0000

    update for build of gfortran_9.2.0b_openmpi_O_develop with hash v8.3.0b10 on hera [ci skip]

--- develop/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/test-gfortran_9.2.0b_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/test-gfortran_9.2.0b_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 21:26:20 UTC 2022
+Thu Mar 24 05:55:18 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0b_openmpi_O.bat_%j.o
@@ -39,7 +39,7 @@ cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh hfe04 /scratch1/NCEPDEV/stmp2/role.esmfmaint/gfortran_9.2.0b_openmpi_O_develop/runpython.sh 2>&1 | tee python_build.log
+ssh hfe06 /scratch1/NCEPDEV/stmp2/role.esmfmaint/gfortran_9.2.0b_openmpi_O_develop/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
