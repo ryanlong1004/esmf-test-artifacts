f6e49e96fe8723c09db412dc1f72290410894813
Ryan Long <ryan.long@noaa.gov>
2022-03-24 06:02:36 -0600

    update for build of gfortran_9.1.0_openmpi_O_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on cheyenne [ci skip]

--- jedwards_pio_update2/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/build-gfortran_9.1.0_openmpi_O.bat
+++ jedwards_pio_update2/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/build-gfortran_9.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 14:18:38 MDT 2022
+Thu Mar 24 06:02:31 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_9.1.0_openmpi_O.bat
 #PBS -l walltime=1:00:00
--- jedwards_pio_update2/cheyenne/intel/18.0.5/g/module/none/out/test-intel_18.0.5_module_g.bat
+++ jedwards_pio_update2/cheyenne/intel/18.0.5/g/module/none/out/test-intel_18.0.5_module_g.bat
@@ -1,24 +1,25 @@
-Thu Mar 24 06:02:31 MDT 2022
+Thu Mar 24 06:01:57 MDT 2022
 #!/bin/sh -l
-#PBS -N test-gfortran_9.1.0_openmpi_O.bat
-#PBS -l walltime=2:00:00
+#PBS -N test-intel_18.0.5_module_g.bat
+#PBS -l walltime=3:00:00
 #PBS -q regular
 #PBS -A p93300606
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/gfortran_9.1.0_openmpi_O_jedwards_pio_update2
-module load gnu/9.1.0 openmpi/4.0.5 netcdf/4.7.3
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_module_g_jedwards_pio_update2
+
+module load python
+module load intel/18.0.5  netcdf/4.6.3
 module list >& module-test.log
 
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_9.1.0_openmpi_O_jedwards_pio_update2
-export ESMF_COMPILER=gfortran
-export ESMF_COMM=openmpi
-export ESMF_BOPT='O'
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_module_g_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=module
+export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
