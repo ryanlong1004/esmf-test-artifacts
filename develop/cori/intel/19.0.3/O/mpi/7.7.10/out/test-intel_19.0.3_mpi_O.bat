82114105d9e2693ac0d3360763172c115c594436
Rocky Dunlap <dunlap@ucar.edu>
2022-03-24 02:34:16 -0700

    update for build of intel_19.0.3_mpi_O_develop with hash v8.3.0b10 on cori [ci skip]

--- develop/cori/intel/19.0.3/O/mpi/7.7.10/out/test-intel_19.0.3_mpi_O.bat
+++ develop/cori/intel/19.0.3/O/mpi/7.7.10/out/test-intel_19.0.3_mpi_O.bat
@@ -1,4 +1,4 @@
-Wed Mar 23 03:58:20 PDT 2022
+Thu Mar 24 02:34:16 PDT 2022
 #!/bin/sh -l
 #SBATCH --account=e3sm
 #SBATCH -o test-intel_19.0.3_mpi_O.bat_%j.o
@@ -10,6 +10,8 @@ Wed Mar 23 03:58:20 PDT 2022
 #SBATCH --ntasks-per-node=32
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
+
+module unload darshan
 module load intel/19.0.3.199 cray-mpich/7.7.10 cray-netcdf/4.6.3.2
 module list >& module-test.log
 
@@ -18,6 +20,8 @@ export ESMF_NETCDF=nc-config
 
 export ESMF_NETCDF_LIBS="-lnetcdf"
 export ESMF_NETCDFF_LIBS="-lnetcdff"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/cray/pe/hdf5/1.10.5.2/INTEL/19.0/lib/pkgconfig
+export ESMF_PNETCDF="pnetcdf-config"
 export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.0.3_mpi_O_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=mpi
