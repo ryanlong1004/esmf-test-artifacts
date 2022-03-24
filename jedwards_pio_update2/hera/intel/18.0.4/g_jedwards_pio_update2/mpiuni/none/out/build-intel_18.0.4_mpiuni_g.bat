620f8b93a80f85caa9d7e1ba22bb85069b2c339c
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-24 23:13:11 +0000

    update for build of intel_18.0.4_mpiuni_g_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on v8.3.0b09-103-gbb860f2 [ci skip]

---
+++ jedwards_pio_update2/hera/intel/18.0.4/g_jedwards_pio_update2/mpiuni/none/out/build-intel_18.0.4_mpiuni_g.bat
@@ -0,0 +1,30 @@
+Thu Mar 24 23:13:08 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-intel_18.0.4_mpiuni_g.bat_%j.o
+#SBATCH -e build-intel_18.0.4_mpiuni_g.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+export ESMF_MPIRUN=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_mpiuni_g_jedwards_pio_update2/src/Infrastructure/stubs/mpiuni/mpirun
+module load intel/18.0.5.274  netcdf-hdf5parallel/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_mpiuni_g_jedwards_pio_update2
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
