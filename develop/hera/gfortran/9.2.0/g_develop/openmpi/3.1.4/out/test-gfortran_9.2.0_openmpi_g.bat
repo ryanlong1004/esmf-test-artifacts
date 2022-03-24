4160e83ec60fd29ea6d484282ebda5b345386954
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-24 23:15:37 +0000

    update for build of gfortran_9.2.0_openmpi_g_develop with hash v8.3.0b10-1-gffc7863 on v8.3.0b10-1-gffc7863 [ci skip]

---
+++ develop/hera/gfortran/9.2.0/g_develop/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
@@ -0,0 +1,51 @@
+Thu Mar 24 23:15:34 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o test-gfortran_9.2.0_openmpi_g.bat_%j.o
+#SBATCH -e test-gfortran_9.2.0_openmpi_g.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+module load gnu/9.2.0 openmpi/3.1.4 netcdf-hdf5parallel/4.7.4
+module load hdf5/1.10.5 
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF=split
+export ESMF_NETCDF_INCLUDE=$NETCDF/include
+export ESMF_NETCDF_LIBPATH=$NETCDF/lib
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5 $HDF5ExtraLibs"
+export ESMF_NETCDF=nc-config
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/gfortran_9.2.0_openmpi_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
+
+cd ../src/addon/ESMPy
+
+export PATH=$PATH:$HOME/.local/bin
+python3 setup.py build 2>&1 | tee python_build.log
+ssh hfe04 /scratch1/NCEPDEV/stmp2/role.esmfmaint/gfortran_9.2.0_openmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
+python3 setup.py test 2>&1 | tee python_test.log
+python3 setup.py test_examples 2>&1 | tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
