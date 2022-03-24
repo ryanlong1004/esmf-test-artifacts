8bd6b7713b138e14d6b96f64e2a1f40804302a33
Ryan Long <ryan.long@noaa.gov>
2022-03-24 17:23:22 -0600

    update for build of gfortran_10.1.0_mpt_g_develop with hash v8.3.0b10-1-gffc7863 on v8.3.0b10-1-gffc7863 [ci skip]

---
+++ develop/cheyenne/gfortran/10.1.0/g_develop/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
@@ -0,0 +1,27 @@
+Thu Mar 24 17:23:19 MDT 2022
+#!/bin/sh -l
+#PBS -N build-gfortran_10.1.0_mpt_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/gfortran_10.1.0_mpt_g_develop
+module load gnu/10.1.0 mpt/2.23 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_10.1.0_mpt_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
