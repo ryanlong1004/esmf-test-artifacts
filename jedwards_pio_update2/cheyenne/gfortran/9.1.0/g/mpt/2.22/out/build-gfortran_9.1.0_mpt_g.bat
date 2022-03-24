1c0d0fa5945a85b79958a45083b76d1c2ab693ff
Ryan Long <ryan.long@noaa.gov>
2022-03-24 06:11:32 -0600

    update for build of gfortran_9.1.0_mpt_g_jedwards_pio_update2 with hash v8.3.0b09-103-gbb860f2 on cheyenne [ci skip]

---
+++ jedwards_pio_update2/cheyenne/gfortran/9.1.0/g/mpt/2.22/out/build-gfortran_9.1.0_mpt_g.bat
@@ -0,0 +1,26 @@
+Thu Mar 24 06:11:31 MDT 2022
+#!/bin/sh -l
+#PBS -N build-gfortran_9.1.0_mpt_g.bat
+#PBS -l walltime=1:00:00
+#PBS -l walltime=2:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/rlong/esmf-testing/gfortran_9.1.0_mpt_g_jedwards_pio_update2
+module load gnu/9.1.0 mpt/2.22 netcdf/4.7.3
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_9.1.0_mpt_g_jedwards_pio_update2
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 36 2>&1| tee build_$JOBID.log
+
