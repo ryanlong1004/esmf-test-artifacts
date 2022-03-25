279601ca9fc3e5f1bd4b90a52dc9fa7b5059faeb
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-25 01:36:25 -0400

    update for build of pgi_20.4_mpiuni_O_develop with hash v8.3.0b10-1-gffc7863 on discover [ci skip]

---
+++ develop/discover/pgi/20.4/O/mpiuni/none/out/build-pgi_20.4_mpiuni_O.bat
@@ -0,0 +1,27 @@
+Fri Mar 25 01:36:12 EDT 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o build-pgi_20.4_mpiuni_O.bat_%j.o
+#SBATCH -e build-pgi_20.4_mpiuni_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_20.4_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+module load comp/pgi/20.4  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_20.4_mpiuni_O_develop
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 2>&1| tee build_$JOBID.log
+
---
+++ develop/discover/pgi/20.4/O/mpiuni/none/out/test-pgi_20.4_mpiuni_O.bat
@@ -0,0 +1,32 @@
+Fri Mar 25 01:36:12 EDT 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o test-pgi_20.4_mpiuni_O.bat_%j.o
+#SBATCH -e test-pgi_20.4_mpiuni_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/pgi/20.4  
+
+module list >& module-test.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_20.4_mpiuni_O_develop
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
