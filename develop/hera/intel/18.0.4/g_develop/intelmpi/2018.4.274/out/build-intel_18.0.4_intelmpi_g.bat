2c5f7ea5d91b413a1ea96bc88ada052d06a1769d
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-25 01:10:33 +0000

    update for build of intel_18.0.4_intelmpi_g_develop with hash v8.3.0b10-1-gffc7863 on v8.3.0b10-1-gffc7863 [ci skip]

---
+++ develop/hera/intel/18.0.4/g_develop/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_g.bat
@@ -0,0 +1,30 @@
+Fri Mar 25 01:10:30 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-intel_18.0.4_intelmpi_g.bat_%j.o
+#SBATCH -e build-intel_18.0.4_intelmpi_g.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+module load intel/18.0.5.274 impi/2018.4.274 netcdf-hdf5parallel/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_intelmpi_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
