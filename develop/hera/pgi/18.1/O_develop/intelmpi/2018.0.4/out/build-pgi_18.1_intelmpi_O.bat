d0d8285b4322a771d578f8026435ce5a93c5126d
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-25 00:29:48 +0000

    update for build of pgi_18.1_intelmpi_O_develop with hash v8.3.0b10-1-gffc7863 on v8.3.0b10-1-gffc7863 [ci skip]

---
+++ develop/hera/pgi/18.1/O_develop/intelmpi/2018.0.4/out/build-pgi_18.1_intelmpi_O.bat
@@ -0,0 +1,29 @@
+Fri Mar 25 00:29:45 UTC 2022
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o build-pgi_18.1_intelmpi_O.bat_%j.o
+#SBATCH -e build-pgi_18.1_intelmpi_O.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+module load cmake
+export ESMF_MPIRUN=mpirun.srun
+module load pgi/18.10 impi/2018.0.4 
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/role.esmfmaint/pgi_18.1_intelmpi_O_develop
+export ESMF_COMPILER=pgi
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 2>&1| tee build_$JOBID.log
+
