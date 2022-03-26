df5c9a3f900b90737282b3d0e142b904aa539cff
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-26 03:19:29 -0400

    update for test of gfortran_10.1.0_intelmpi_g_develop with hash v8.3.0b10-105-ga5295e3 on discover [ci skip]

--- develop/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/test-gfortran_10.1.0_intelmpi_g.bat
+++ develop/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/test-gfortran_10.1.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Mar 26 02:12:17 EDT 2022
+Sat Mar 26 03:19:01 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_10.1.0_intelmpi_g.bat_%j.o
--- develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
+++ develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Mar 26 02:12:10 EDT 2022
+Sat Mar 26 03:19:25 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_g.bat_%j.o
