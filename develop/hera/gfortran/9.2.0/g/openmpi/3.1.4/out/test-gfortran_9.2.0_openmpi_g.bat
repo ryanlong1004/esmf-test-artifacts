9b4fb96978209193619fa6d56a96a51fbc1f282b
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-26 06:07:32 +0000

    update for build of intel_18.0.4_intelmpi_g_develop with hash v8.3.0b10-105-ga5295e3 on v8.3.0b10-105-ga5295e3 [ci skip]

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Mar 26 06:05:13 UTC 2022
+Sat Mar 26 06:06:47 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_openmpi_g.bat_%j.o
--- develop/hera/intel/18.0.4/g/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_g.bat
+++ develop/hera/intel/18.0.4/g/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Mar 26 05:20:30 UTC 2022
+Sat Mar 26 06:07:30 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-intel_18.0.4_intelmpi_g.bat_%j.o
@@ -39,7 +39,7 @@ cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh hfe04 /scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_intelmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
+ssh hfe06 /scratch1/NCEPDEV/stmp2/role.esmfmaint/intel_18.0.4_intelmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
