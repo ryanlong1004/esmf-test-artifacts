a9276eb4b2f26139f31a35c5c7f80840d98ee5de
Mark A Potts <Mark.Potts@noaa.gov>
2022-03-27 15:35:49 +0000

    update for build of gfortran_10.3.0_mpi_O_develop with hash v8.3.0b10-105-ga5295e3 on acorn [ci skip]

--- develop/acorn/gfortran/10.3.0/O/mpi/8.1.7/out/build-gfortran_10.3.0_mpi_O.bat
+++ develop/acorn/gfortran/10.3.0/O/mpi/8.1.7/out/build-gfortran_10.3.0_mpi_O.bat
@@ -1,4 +1,4 @@
-Mon 21 Mar 01:30:03 UTC 2022
+Sun 27 Mar 15:35:48 UTC 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_10.3.0_mpi_O.bat
 #PBS -l walltime=1:00:00
--- develop/acorn/gfortran/10.3.0/O/mpi/8.1.7/out/test-gfortran_10.3.0_mpi_O.bat
+++ develop/acorn/gfortran/10.3.0/O/mpi/8.1.7/out/test-gfortran_10.3.0_mpi_O.bat
@@ -1,4 +1,4 @@
-Mon 21 Mar 01:30:03 UTC 2022
+Sun 27 Mar 15:35:48 UTC 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.3.0_mpi_O.bat
 #PBS -l walltime=1:00:00
@@ -43,7 +43,7 @@ cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_O_develop/runpython.sh 2>&1 | tee python_build.log
+ssh alogin02 /lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_O_develop/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
