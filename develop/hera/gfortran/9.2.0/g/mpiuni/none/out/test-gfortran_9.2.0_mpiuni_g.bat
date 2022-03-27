f427c4614bcaa261d0fae30af5effc7b74f17424
Role.Esmfmaint <esmfmaint@gmail.com>
2022-03-27 06:04:44 +0000

    update for build of gfortran_9.2.0_mpiuni_g_develop with hash v8.3.0b10-105-ga5295e3 on v8.3.0b10-105-ga5295e3 [ci skip]

--- develop/hera/gfortran/9.2.0/g/mpiuni/none/out/test-gfortran_9.2.0_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0/g/mpiuni/none/out/test-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Mar 26 06:04:10 UTC 2022
+Sun Mar 27 06:04:42 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_mpiuni_g.bat_%j.o
--- develop/hera/intel/18.0.4/g/mpiuni/none/out/test-intel_18.0.4_mpiuni_g.bat
+++ develop/hera/intel/18.0.4/g/mpiuni/none/out/test-intel_18.0.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sun Mar 27 06:02:48 UTC 2022
+Sun Mar 27 06:04:22 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-intel_18.0.4_mpiuni_g.bat_%j.o
